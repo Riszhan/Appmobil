50f101c1322def282c858550b185e759030380c6
Riszhan <riszhan.n@students.amikom.ac.id>
2020-12-11 21:25:14 +0700

    Tugas pcs

---
+++ app/src/main/java/com/appmobil/app/data/model/AuthUser.kt
@@ -0,0 +1,11 @@
+package com.appmobil.app.data.model
+
+import java.util.*
+
+data class AuthUser(
+    val id: Int = 0,
+    val email: String = "",
+    val password: String = "",
+    val FullName: String = "",
+
+)
---
+++ app/src/main/java/com/appmobil/app/ui/auth/LoginFragment.kt
@@ -0,0 +1,59 @@
+package com.appmobil.app.ui.auth
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.appmobil.app.R
+import com.appmobil.app.data.repository.AuthRepository
+import com.appmobil.app.databinding.FragmentLoginBinding
+import kotlin.math.log
+
+class LoginFragment : Fragment() {
+    val parent: AuthActivity by lazy { activity as AuthActivity }
+    val viewModel: AuthViewModel by lazy { AuthViewModel(AuthRepository(parent)) }
+    lateinit var binding: FragmentLoginBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+        observe()
+
+    }
+
+    private fun init() {
+        binding.ButtonRegister.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToRegisterrFragment())
+        }
+    }
+
+    private fun observe() {
+        viewModel.authLogin.observe(viewLifecycleOwner) {
+            if (it.isConsumed) {
+                Log.i("Login", "isConsumed")
+            } else if (!it.isSuccsess) {
+                Toast.makeText(parent, it.message, Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(parent, it.message, Toast.LENGTH_SHORT).show()
+                parent.onSuccess(it.data)
+            }
+
+           it.isConsumed = true
+        }
+    }
+}
